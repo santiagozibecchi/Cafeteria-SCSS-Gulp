e7f4f3656ca9403ac432b3dda0a5009889180304
Santiago <santiago.zibe@gmail.com>
2022-07-12 10:35:07 -0300

    minificacion de imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 const { src, dest, watch, series, parallel } = require('gulp');
+
+// CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer')
+const autoprefixer = require('autoprefixer');
+
+// IMAGENES
+const imagemin = require('gulp-imagemin');
 
 function css(done) {
      // compilar sass
@@ -15,14 +20,23 @@ function css(done) {
      done();
 }
 
+function imagenes() {
+     return src('src/img/**/*')
+          .pipe(imagemin({ optimizationLevel: 3 }))
+          .pipe(dest('build/img'))
+}
+
 function dev() {
      watch('src/scss/**/*.scss', css)
+     watch('src/img/**/*', imagenes)
+
      // watch('src/scss/app.scss', css)
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev)
+exports.imagenes = imagenes;
+exports.default = series(imagenes, css, dev)
 
 // series -> Se inicia una tarea, y hasta que finaliza, inicia la siguiente
 // parallel -> Todas inician al mismo tiempo
