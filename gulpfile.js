31d2fcf3f0fb5b7d8ef2184dd0438ceb2d746995
Santiago <santiago.zibe@gmail.com>
2022-07-13 17:40:53 -0300

    cafe pages terminado

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
 // IMAGENES
-const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const avif = require('gulp-avif');
+// const imagemin = require('gulp-imagemin');
+// const webp = require('gulp-webp');
+// const avif = require('gulp-avif');
 
 function css(done) {
      // compilar sass
@@ -22,44 +22,44 @@ function css(done) {
      done();
 }
 
-function versionWebp() {
-     const opciones = {
-          quality: 50
-     }
-     return src('src/img/**/*.{png,jpg}')
-          .pipe(webp(opciones))
-          .pipe(dest('build/img'))
-}
+// function versionWebp() {
+//      const opciones = {
+//           quality: 50
+//      }
+//      return src('src/img/**/*.{png,jpg}')
+//           .pipe(webp(opciones))
+//           .pipe(dest('build/img'))
+// }
 
-function versionAvif() {
-     const opciones = {
-          quality: 50
-     }
+// function versionAvif() {
+//      const opciones = {
+//           quality: 50
+//      }
 
-     return src('src/img/**/*.{png,jpg}')
-          .pipe(avif(opciones))
-          .pipe(dest('build/img'))
-}
+//      return src('src/img/**/*.{png,jpg}')
+//           .pipe(avif(opciones))
+//           .pipe(dest('build/img'))
+// }
 
-function imagenes() {
-     return src('src/img/**/*')
-          .pipe(imagemin({ optimizationLevel: 3 }))
-          .pipe(dest('build/img'))
-}
+// function imagenes() {
+//      return src('src/img/**/*')
+//           .pipe(imagemin({ optimizationLevel: 3 }))
+//           .pipe(dest('build/img'))
+// }
 
 function dev() {
      watch('src/scss/**/*.scss', css)
-     watch('src/img/**/*', imagenes)
+     // watch('src/img/**/*', imagenes)
 
      // watch('src/scss/app.scss', css)
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.imagenes = imagenes;
-exports.versionWebp = versionWebp;
-exports.versionAvif = versionAvif;
-exports.default = series(imagenes, versionWebp, versionAvif, css, dev)
+// exports.imagenes = imagenes;
+// exports.versionWebp = versionWebp;
+// exports.versionAvif = versionAvif;
+exports.default = series(css, dev)
 
 // series -> Se inicia una tarea, y hasta que finaliza, inicia la siguiente
 // parallel -> Todas inician al mismo tiempo
